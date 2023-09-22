6851197bf7a924e36e5b15d4249b716aecfbe1a4
abdoshahin1 <abdoahmadm55@gmail.com>
2023-09-22 14:23:58 +0300

    solved tasks and file that explane the concept of oop

--- task16.py
+++ task16.py
@@ -3,6 +3,6 @@
 # print(f"Random Even Number Between 2 And 10 => {randrange(2, 10 ,2)}")
 # print(f"Random Odd Number Between 1 And 9 => {randrange(1, 9, 2)}")
 # print(dir(random))
-from my_mod import say_welcome as new_welcome
-# say_welcome("ali")
-new_welcome("omer")
+# from my_mod import say_welcome as new_welcome
+# # say_welcome("ali")
+# new_welcome("omer")
