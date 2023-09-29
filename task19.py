f1fbc9d20eb116e5b357d9b23a91414e0a423279
abdoshahin1 <abdoahmadm55@gmail.com>
2023-09-29 03:34:49 +0300

    i am tired

---
+++ task19.py
@@ -0,0 +1,67 @@
+'''
+this is hard task because i search all time
+'''
+# my_list = ["E", "Z", "R", 1, 2, 3]
+# my_tuple = ("L", "E", "O")
+# my_data = []
+
+# for data in zip(my_list, my_tuple):
+#     my_data.extend(data)
+#     final_string = ("".join(my_data) ).capitalize()
+
+# print(final_string) # Elzero
+
+# my_list1 = ["E", "L", "Z", "E", "R", "O", 1, 2]
+# my_tuple = ("E", "Z", "R", 1, 2, "E", "R", "O")
+# my_list2 = ("L", "E", "O", 1, 2, "E", "R", "O")
+# my_data = []
+
+# for item1, item2, item3 in zip(my_list1, my_tuple, my_list2):
+#     my_data.append(item1)
+#     if len(my_data) == len("Elzero"):
+#         final_string = (''.join(my_data)).capitalize()
+
+# print(final_string)
+
+
+# from PIL import Image
+# my_image = Image.open(r"D:\backend\python\photo\elzero-pillow.png")
+
+# image_crop = (400, 0, 800, 400)
+
+# new_image = my_image.crop(image_crop)
+
+# new_image = new_image.convert("L")
+
+# new_crop = (0, 400, 1200, 800)
+# new_imaged = my_image.crop(new_crop)
+# new_imaged = new_imaged.convert("L")
+# new_imaged = new_imaged.transpose(3)
+# new_imaged.save("test2.png")
+# new_imaged.show()
+
+
+# def say_hello_to(name):
+#     """
+#     "parameter({name}) => Person Name"
+#     "Function To Say Hello To Anyone"
+#     """
+#     return f"hello, {name}"
+
+
+# print(say_hello_to("Osama"))
+
+# print(say_hello_to.__doc__)
+
+
+
+# myFriends = ["Ahmed", "Osama", "Sayed"]
+
+# def say_hello(some_peoples) -> list:
+#     '''
+#     this is a function
+#     '''
+#     for some_one in some_peoples:
+#         print(f"Hello {some_one}")
+
+# say_hello(myFriends)
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-print(test_time.strftime("%d - %b - %Y"))
