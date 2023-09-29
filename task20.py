50019e091e584beebc0b6bd4fda40550902bb0eb
abdoshahin1 <abdoahmadm55@gmail.com>
2023-09-29 04:05:02 +0300

    review the error handling

---
+++ task20.py
@@ -0,0 +1,27 @@
+# NUM = input("Add Your Number ")
+# if len(NUM) > 1:
+#     raise IndexError("Only One Character Allowed")
+# elif NUM.isalpha():
+#     raise Exception("Only Numbers Allowed")
+# elif int(NUM) == 0:
+#     raise ValueError("Number Must Be Larger Than 0")
+# else:
+#     print("####################")
+#     print(f"this is the number: {NUM}")
+#     print("####################")
+# LETTER = input("Add Letter From A to Z: ")
+# try:
+#     if len(LETTER) > 1:
+#         raise NameError
+#     elif LETTER.isupper():
+#         raise IndexError
+# except NameError:
+#     print("You Must Write One Character Only")
+# except IndexError:
+#     print("The Letter Not In A - Z")
+# else:
+#     print(f"you typed {LETTER}")
+# def calculate(num1, num2) -> int:
+#   return num1 + num2
+
+# print(calculate(20, 30))
